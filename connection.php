b78966196b227edfbe22e305fb4c9c65584b07c4
Marcos Mariscal <marcos.mariscal@icloud.com>
2016-01-20 15:54:54 +0100

    Modificaciones

--- connection.php
+++ connection.php
@@ -4,8 +4,12 @@
 
     $mysqli = new mysqli($host, $user, $pwd, $db);
 
+    /* Comprobamos mediante el procedimiento connect_errno si tiene algún error de conexión, y si es así,
+     *  nos lo muestra por pantalla
+     */
     if ($mysqli->connect_errno){
         echo "Fallo al conectar a MySQL: (".$mysqli->connect_errno.")".$mysqli->connect_error;
+        exit();
     }else{
         echo "Conectado a la base de datos con éxito <br/>";
     }
--- mostrar-datos.php
+++ mostrar-datos.php
@@ -6,13 +6,6 @@
     // Instanciamos el objeto $mysqli con los datos que nos vienen de connection.php
     $mysqli = new mysqli($host, $user, $pwd, $db);
 
-    /* Comprobamos mediante el procedimiento connect_errno si tiene algún error de conexión, y si es así,
-     *  nos lo muestra por pantalla
-     */
-    if ($mysqli->connect_errno){
-        echo "Fallo al conectar a MySQL: ".$mysqli->connect_error;
-    }
-
     $result = $mysqli->query("SELECT * FROM PIEZA");
     $row = $result->fetch_assoc();
     echo $row['MODELO'];
