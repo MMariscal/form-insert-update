e0ef82e7d1237a2c23eccfee88c645a937b7ddab
Marcos Mariscal <marcos.mariscal@icloud.com>
2016-01-21 17:23:16 +0100

    Retoques estéticos

--- nuevo.php
+++ nuevo.php
@@ -7,48 +7,55 @@
 
 		<center><h1>Nuevo Item</h1></center>
 
-		<form name="nuevo_registro" method="POST" action="guarda_item.php">
-			<table width="50%">
-				<tr>
-					<td width="20"><b>MODELO</b></td>
-					<td width="30"><input type="text" name="modelo" size="75" /></td>
-				</tr>
-				<tr>
-					<td><b>MEDIDAS</b></td>
-					<td><input type="text" name="medidas" size="75" /></td>
-				</tr>
-				<tr>
-					<td><b>USO</b></td>
-					<td><input type="text" name="uso" size="75" /></td>
-				</tr>
-				<tr>
-					<td width="20"><b>SERIE</b></td>
-					<td width="30"><input type="text" name="serie" size="75" /></td>
-				</tr>
-				<tr>
-					<td><b>COLOR</b></td>
-					<td><input type="text" name="color" size="75" /></td>
-				</tr>
-				<tr>
-					<td><b>APLICACION</b></td>
-					<td><input type="text" name="aplicacion" size="75" /></td>
-				</tr>
-				<tr>
-					<td width="20"><b>ESTILO</b></td>
-					<td width="30"><input type="text" name="estilo" size="75" /></td>
-				</tr>
-				<tr>
-					<td><b>IMAGEN</b></td>
-					<td><input type="text" name="imagen" size="75" /></td>
-				</tr>
-				<tr>
-					<td><b>OTROS</b></td>
-					<td><input type="text" name="otros" size="75" /></td>
-				</tr>
-				<tr>
-					<td colspan="2"><center><input type="submit" name="eviar" value="Insertar" /></center></td>
-				</tr>
-			</table>
-		</form>
+		<center>
+            <form name="nuevo_registro" method="POST" action="guarda_item.php">
+                <fieldset>
+                    <table width="50%">
+                        <tr>
+                            <td width="20"><b>MODELO</b></td>
+                            <td width="30"><input type="text" name="modelo" size="75" /></td>
+                        </tr>
+                        <tr>
+                            <td width="20"><b>MEDIDAS</b></td>
+                            <td width="30"><input type="text" name="medidas" size="75" /></td>
+                        </tr>
+                        <tr>
+                            <td width="20"><b>USO</b></td>
+                            <td width="30"><input type="text" name="uso" size="75" /></td>
+                        </tr>
+                        <tr>
+                            <td width="20"><b>SERIE</b></td>
+                            <td width="30"><input type="text" name="serie" size="75" /></td>
+                        </tr>
+                        <tr>
+                            <td width="20"><b>COLOR</b></td>
+                            <td width="30"><input type="text" name="color" size="75" /></td>
+                        </tr>
+                        <tr>
+                            <td width="20"><b>APLICACION</b></td>
+                            <td width="30"><input type="text" name="aplicacion" size="75" /></td>
+                        </tr>
+                        <tr>
+                            <td width="20"><b>ESTILO</b></td>
+                            <td width="30"><input type="text" name="estilo" size="75" /></td>
+                        </tr>
+                        <tr>
+                            <td width="20"><b>IMAGEN</b></td>
+                            <td width="30"><input type="text" name="imagen" size="75" /></td>
+                        </tr>
+                        <tr>
+                            <td width="20"><b>OTROS</b></td>
+                            <td width="30"><input type="text" name="otros" size="75" /></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td>
+                                <button type="submit" name="eviar" value="Insertar">Insertar</button>
+                            </td>
+                        </tr>
+                    </table>
+                </fieldset>
+            </form>
+		</center>
 	</body>
 </html>
